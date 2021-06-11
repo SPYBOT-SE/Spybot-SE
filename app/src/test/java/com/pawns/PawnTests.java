e5f6edb1b213949c9a23b30d9bc333e083f68579
steiditi <Timo.Steidinger@gmx.de>
2021-06-11 14:20:39 +0200

    Testing Error ...

--- app/src/test/java/com/pawns/PawnTests.java
+++ app/src/test/java/com/pawns/PawnTests.java
@@ -3,6 +3,7 @@ package com.pawns;
 import com.level.Board;
 import com.level.Field;
 import com.model.Direction;
+import com.pawns.university.Bug;
 import com.pawns.university.Dumbbell;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,34 +24,41 @@ public class PawnTests {
 
     private Board board;
 
-    private Field field1;
+    private Field fieldSpawnP1;
     private Field field2;
     private Field field3;
+    private Field field4;
 
-    private Pawn p;
+    private Pawn p1;
+    private Pawn p2;
 
     @BeforeEach
     void iniBoard() {
         board = new Board(testLvl);
 
-        field1 = board.getField((short) 0, (short) 0);
+        fieldSpawnP1 = board.getField((short) 0, (short) 0);
         field2 = board.getField((short) 0, (short) 1);
         field3 = board.getField((short) 1, (short) 0);
+        field4 = board.getField((short) 2,(short) 0);
 
-        p = new Dumbbell();
-        board.pawnsOnBoard.add(p);
-        p.createSegment(field1, BodyType.Head);
+        p1 = new Dumbbell();
+        board.pawnsOnBoard.add(p1);
+        p1.createSegment(fieldSpawnP1, BodyType.Head);
+
+        p2 = new Bug();
+        board.pawnsOnBoard.add(p2);
+        p2.createSegment(field4, BodyType.Head);
     }
 
     @Test
     void moveOneRight() {
 
-        p.move(field1, field2, Direction.RIGHT );
+        p1.move(fieldSpawnP1, field2, Direction.RIGHT );
 
-        Assertions.assertNotNull(field1.getSegment());
+        Assertions.assertNotNull(fieldSpawnP1.getSegment());
         Assertions.assertNotNull(field2.getSegment());
 
-        Assertions.assertEquals(field1.getSegment().getBodyType(), BodyType.TailRight);
+        Assertions.assertEquals(fieldSpawnP1.getSegment().getBodyType(), BodyType.TailRight);
         Assertions.assertEquals(field2.getSegment().getBodyType(), BodyType.Head);
     }
 
@@ -58,12 +66,28 @@ public class PawnTests {
     @Test
     void moveOneDown() {
 
-        p.move(field1, field3, Direction.DOWN );
+        p1.move(fieldSpawnP1, field3, Direction.DOWN );
 
-        Assertions.assertNotNull(field1.getSegment());
+        Assertions.assertNotNull(fieldSpawnP1.getSegment());
         Assertions.assertNotNull(field3.getSegment());
 
-        Assertions.assertEquals(field1.getSegment().getBodyType(), BodyType.TailDown);
+        Assertions.assertEquals(fieldSpawnP1.getSegment().getBodyType(), BodyType.TailDown);
         Assertions.assertEquals(field3.getSegment().getBodyType(), BodyType.Head);
+    }
+
+    @Test
+    void attackTest() {
+
+        p2.move(field4, field3, Direction.UP);
+
+        Assertions.assertNotNull(field4.getSegment());
+        Assertions.assertNotNull(field3.getSegment());
+        Assertions.assertNotNull(fieldSpawnP1.getSegment());
+
+        p2.attack1(null, fieldSpawnP1);
+
+        Assertions.assertNull(fieldSpawnP1.getSegment());
+
+
     }
 }
